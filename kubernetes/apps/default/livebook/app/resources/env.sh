d07307a61a29a1a338f3cb621f4a9db21b91d834
brett <brettinternet@gmail.com>
2024-06-07 13:23:35 -0600

    add netshoot debugger, fix livebook node discovery

--- kubernetes/apps/default/livebook/app/resources/env.sh
+++ kubernetes/apps/default/livebook/app/resources/env.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 export LIVEBOOK_IP="${POD_IP:-$(hostname -i)}"
-export LIVEBOOK_NODE="livebook_server@$LIVEBOOK_IP"
-export RELEASE_NODE="$LIVEBOOK_NODE"
+export LIVEBOOK_NODE="livebook@$LIVEBOOK_IP"
