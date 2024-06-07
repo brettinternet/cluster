46550f282bc1017867d98feba7503fc9b7f565e2
brett <brettinternet@gmail.com>
2024-06-07 11:49:07 -0600

    add livebook headless service

--- kubernetes/apps/default/livebook/app/resources/env.sh
+++ kubernetes/apps/default/livebook/app/resources/env.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-export LIVEBOOK_CLUSTER="dns:QUERY"
-export RELEASE_DISTRIBUTION="name"
 export LIVEBOOK_IP="${POD_IP:-$(hostname -i)}"
-export POD_A_RECORD=$(echo $LIVEBOOK_IP | sed 's/\./-/g')
-export LIVEBOOK_NODE="${HOSTNAME:-livebook}@$POD_A_RECORD.$NAMESPACE.pod.cluster.local"
+export LIVEBOOK_NODE="livebook_server@$LIVEBOOK_IP"
 export RELEASE_NODE="$LIVEBOOK_NODE"
